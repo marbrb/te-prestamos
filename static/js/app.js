e6b5bc651a01c5565e36ac999d787d54b7a72898
Miguel Rodriguez <marbrb1@gmail.com>
2017-05-22 20:22:36 -0500

    Se modífico la forma en la que se inyectan los modulos, y se establecio el modelo de inicio de sesión.

--- static/js/app.js
+++ static/js/app.js
@@ -2,29 +2,36 @@
   'use strict';
 
   angular.module('CREED',  ['ngRoute'])
-  .controller('MainController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
-  })
-  .controller('LoginController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
-    console.log('jeje');
-  })
-  .config(function($routeProvider, $locationProvider) {
-  $routeProvider
-   .when('/', {
-    templateUrl: 'templates/login.html',
-    controller: 'LoginController',
-    resolve: {
-      // I will cause a 1 second delay
-      delay: function($q, $timeout) {
-        var delay = $q.defer();
-        $timeout(delay.resolve, 1000);
-        return delay.promise;
+  .controller('MainController', [
+    '$scope',
+    '$http',
+    '$location',
+    function($scope, $http, $location) {
+
+  }])
+  .controller('LoginController', [
+    '$scope',
+    '$http',
+    '$location',
+    function($scope, $http, $location) {
+      $scope.submit = function() {
+        console.log($scope.user);
+        //console.log($http);
       }
-    }
-  })});
+    }])
+    .config(function($routeProvider, $locationProvider) {
+      $routeProvider
+      .when('/', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController',
+        resolve: {
+          // I will cause a 1 second delay
+          delay: function($q, $timeout) {
+            var delay = $q.defer();
+            $timeout(delay.resolve, 1000);
+            return delay.promise;
+          }
+        }
+      })
+    });
 })(window.angular);
