fb4b3e03fe7fc0a523b2704650317f9fb60f281d
Miguel Rodriguez <marbrb1@gmail.com>
2017-05-25 15:03:38 -0500

    archivo central de la aplicacion

---
+++ src/app.py
@@ -0,0 +1,45 @@
+from uuid import uuid4
+from tornado.ioloop import IOLoop
+from login import LoginHandler, BaseHandler
+from creditos import CreditosHandler
+from pagos import PagosHandler
+from tornado import web
+import os
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        self.render("index.html")
+
+class Application:
+    def __init__(self):
+        settings = {
+            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "login_url": "/login",
+            "xsrf_cookies": True,
+        }
+
+        self.app = web.Application([
+                    web.url(r"/$", MainHandler),
+                    web.url(r"/creditos", CreditosHandler),
+                    web.url(r"/pagos/([0-9]+)", PagosHandler),
+
+                    #quejeso
+                    web.url(r"/login", LoginHandler),
+                    (r"/static/(.*)", web.StaticFileHandler, {"path": "../static",
+                                                              "default_filename": "index.html"},),
+        ], cookie_secret=str(uuid4()), deafult_host='10.20.232.35')
+
+    def run(self):
+        self.app.settings["template_path"] = "../"
+        self.app.listen(8888)
+        IOLoop.current().start()
+
+
+if __name__ == '__main__':
+    try:
+        applicaction = Application()
+        applicaction.run()
+    except KeyboardInterrupt:
+        print("Server Stoped.")
+        exit(0)
