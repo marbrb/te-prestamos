01be6ed94710c9977c25907f9a21a79c89a83129
Miguel Rodriguez <marbrb1@gmail.com>
2017-05-25 09:16:28 -0500

    se realiza script que genera la base de datos

---
+++ base_datos.sql
@@ -0,0 +1,45 @@
+create table clientes (
+  cliente_id INTEGER UNSIGNED PRIMARY KEY,
+  nombre VARCHAR(60) NOT NULL,
+  telefono INTEGER UNIQUE NOT NULL,
+  email VARCHAR(30) UNIQUE NOT NULL,
+  ciudad VARCHAR(20) NOT NULL,
+  pais VARCHAR(20) NOT NULL,
+  password VARCHAR(40) NOT NULL
+);
+
+create table creditos(
+  id_credito INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  id_cliente INTEGER UNSIGNED NOT NULL,
+  estado ENUM('pendiente','aprobado','denegado') NOT NULL,
+  fecha_solicitud TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  activo BOOLEAN NOT NULL,
+  monto INTEGER UNSIGNED NOT NULL,
+  interes DECIMAL(2,2) NOT NULL,
+  concepto_cliente BOOLEAN NOT NULL
+);
+
+create table pagos (
+  id_pago INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  id_credito INTEGER UNSIGNED NOT NULL,
+  fecha_pago TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  monto INTEGER UNSIGNED NOT NULL,
+  estado ENUM('hecho','pendiente') NOT NULL
+);
+
+create table empresas (
+  id_empresa INTEGER UNSIGNED PRIMARY KEY,
+  id_cliente INTEGER UNSIGNED NOT NULL,
+  nombre VARCHAR(40) NOT NULL,
+  direccion VARCHAR(30) NOT NULL,
+  telefono INTEGER NOT NULL NULL,
+  ciudad VARCHAR(20) NOT NULL,
+  pais VARCHAR(20) NOT NULL
+);
+
+create table cookies (
+  id_cookie INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  id_cliente INTEGER UNSIGNED NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  cookie VARCHAR(40)
+);
